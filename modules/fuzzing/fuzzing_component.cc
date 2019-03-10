c01d5203dfa1453120dc558e02d8d774ba6e0950
Smlight <imzy140@gmail.com>
2019-03-10 14:37:56 +0800

    use another node to create readers and writers

--- modules/fuzzing/fuzzing_component.cc
+++ modules/fuzzing/fuzzing_component.cc
@@ -4,64 +4,109 @@
 
 #include "modules/fuzzing/fuzzing_component.h"
 
-#include "modules/common/adapters/adapter_gflags.h"
-#include "modules/control/common/control_gflags.h"
-#include "modules/common/configs/config_gflags.h"
-#include "modules/common/util/message_util.h"
-#include "modules/map/hdmap/hdmap_util.h"
-#include "modules/map/pnc_map/pnc_map.h"
-#include "modules/planning/common/planning_context.h"
-
 namespace apollo {
 namespace fuzzing {
 
-using apollo::canbus::Chassis;
-using apollo::common::time::Clock;
-using apollo::hdmap::HDMapUtil;
-using apollo::perception::TrafficLightDetection;
-using apollo::relative_map::MapMsg;
-using apollo::routing::RoutingRequest;
-using apollo::routing::RoutingResponse;
-using apollo::planning::PadMessage;
-using apollo::localization::LocalizationEstimate;
-
 bool FuzzingComponent::Init() {
+  fuzzer_node_ = apollo::cyber::CreateNode("fuzzer");
 
-  cyber::ReaderConfig localization_reader_config;
-  localization_reader_config.channel_name = FLAGS_localization_topic;
-  localization_reader_config.pending_queue_size =
-      FLAGS_localization_pending_queue_size;
-  localization_reader_ = node_->CreateReader<LocalizationEstimate>(
-      localization_reader_config,
+  chassis_reader_ = fuzzer_node_->CreateReader<Chassis>(
+      FLAGS_chassis_topic, [this](const std::shared_ptr<Chassis>& chassis) {
+        AINFO << "Received chassis data:\n" << chassis->DebugString();
+      });
+  localization_reader_ = fuzzer_node_->CreateReader<LocalizationEstimate>(
+      FLAGS_localization_topic,
       [this](const std::shared_ptr<LocalizationEstimate>& localization) {
-        AINFO << "Received localization data: "
-              << localization->DebugString();
+        AINFO << "Received localization data:\n" << localization->DebugString();
       });
-  // CHECK(localization_reader_ != nullptr);
-
-  routing_response_reader_ = node_->CreateReader<RoutingResponse>(
-      "/apollo/routing_response",
-      [this](const std::shared_ptr<RoutingResponse>& routing) {
-        AINFO << "Received routing data: "
-              << routing->DebugString();
-        std::lock_guard<std::mutex> lock(mutex_);
-        routing_response_.CopyFrom(*routing);
+  localization_msf_reader_ = fuzzer_node_->CreateReader<LocalizationStatus>(
+      FLAGS_localization_msf_status,
+      [this](const std::shared_ptr<LocalizationStatus>& localization_msf) {
+        AINFO << "Received localization_msf data:\n"
+              << localization_msf->DebugString();
       });
-  traffic_light_reader_ = node_->CreateReader<TrafficLightDetection>(
+  perception_obstacles_reader_ =
+      fuzzer_node_->CreateReader<PerceptionObstacles>(
+          FLAGS_perception_obstacle_topic,
+          [this](const std::shared_ptr<PerceptionObstacles>&
+                     perception_obstacles) {
+            AINFO << "Received perception_obstacles data:\n"
+                  << perception_obstacles->DebugString();
+          });
+  traffic_light_reader_ = fuzzer_node_->CreateReader<TrafficLightDetection>(
       FLAGS_traffic_light_detection_topic,
       [this](const std::shared_ptr<TrafficLightDetection>& traffic_light) {
-        ADEBUG << "Received traffic light data: "
-               << traffic_light->DebugString();
-        std::lock_guard<std::mutex> lock(mutex_);
-        traffic_light_.CopyFrom(*traffic_light);
+        AINFO << "Received traffic_light data:\n"
+              << traffic_light->DebugString();
+      });
+  planning_trajectory_reader_ = fuzzer_node_->CreateReader<ADCTrajectory>(
+      FLAGS_planning_trajectory_topic,
+      [this](const std::shared_ptr<ADCTrajectory>& planning_trajectory) {
+        AINFO << "Received planning_trajectory data:\n"
+              << planning_trajectory->DebugString();
+      });
+  prediction_obstacles_reader_ =
+      fuzzer_node_->CreateReader<PredictionObstacles>(
+          FLAGS_prediction_topic,
+          [this](const std::shared_ptr<PredictionObstacles>&
+                     prediction_obstacles) {
+            AINFO << "Received prediction_obstacles data:\n"
+                  << prediction_obstacles->DebugString();
+          });
+  relative_map_reader_ = fuzzer_node_->CreateReader<MapMsg>(
+      FLAGS_relative_map_topic,
+      [this](const std::shared_ptr<MapMsg>& relative_map) {
+        AINFO << "Received relative_map data:\n" << relative_map->DebugString();
+      });
+  routing_request_reader_ = fuzzer_node_->CreateReader<RoutingRequest>(
+      FLAGS_routing_request_topic,
+      [this](const std::shared_ptr<RoutingRequest>& routing_request) {
+        AINFO << "Received routing_request data:\n"
+              << routing_request->DebugString();
+      });
+  routing_response_reader_ = fuzzer_node_->CreateReader<RoutingResponse>(
+      FLAGS_routing_response_topic,
+      [this](const std::shared_ptr<RoutingResponse>& routing_response) {
+        AINFO << "Received routing_response data:\n"
+              << routing_response->DebugString();
       });
 
+  chassis_writer_ = fuzzer_node_->CreateWriter<Chassis>(FLAGS_chassis_topic);
+  localization_writer_ = fuzzer_node_->CreateWriter<LocalizationEstimate>(
+      FLAGS_localization_topic);
+  localization_msf_writer_ = fuzzer_node_->CreateWriter<LocalizationStatus>(
+      FLAGS_localization_msf_status);
+  perception_obstacles_writer_ =
+      fuzzer_node_->CreateWriter<PerceptionObstacles>(
+          FLAGS_perception_obstacle_topic);
+  traffic_light_writer_ = fuzzer_node_->CreateWriter<TrafficLightDetection>(
+      FLAGS_traffic_light_detection_topic);
+  planning_trajectory_writer_ = fuzzer_node_->CreateWriter<ADCTrajectory>(
+      FLAGS_planning_trajectory_topic);
+  prediction_obstacles_writer_ =
+      fuzzer_node_->CreateWriter<PredictionObstacles>(FLAGS_prediction_topic);
+  relative_map_writer_ =
+      fuzzer_node_->CreateWriter<MapMsg>(FLAGS_relative_map_topic);
+  routing_request_writer_ =
+      fuzzer_node_->CreateWriter<RoutingRequest>(FLAGS_routing_request_topic);
+  routing_response_writer_ =
+      fuzzer_node_->CreateWriter<RoutingResponse>(FLAGS_routing_response_topic);
 
   return true;
 }
 
-bool FuzzingComponent::Proc(const std::shared_ptr<localization::LocalizationEstimate>&
-                localization_estimate) {
+bool FuzzingComponent::Proc(
+    const std::shared_ptr<localization::LocalizationEstimate>&
+        localization_estimate) {
+  static uint32_t seq = 0;
+  ++seq;
+  auto chassis = std::make_shared<Chassis>();
+  auto* header = chassis->mutable_header();
+  header->set_timestamp_sec(Clock::NowInSeconds());
+  header->set_module_name("hello, from the FUZZING modules!");
+  header->set_sequence_num(seq);
+  chassis->set_driving_mode(apollo::canbus::Chassis_DrivingMode_COMPLETE_AUTO_DRIVE);
+  chassis_writer_->Write(chassis);
   return true;
 }
 
--- modules/fuzzing/fuzzing_component.h
+++ modules/fuzzing/fuzzing_component.h
@@ -6,19 +6,41 @@
 
 #include <memory>
 
+#include "gflags/gflags.h"
+#include "modules/common/adapters/adapter_gflags.h"
+#include "modules/control/common/control_gflags.h"
+#include "modules/common/configs/config_gflags.h"
+
+#include "cyber/cyber.h"
 #include "cyber/class_loader/class_loader.h"
 #include "cyber/component/component.h"
 #include "cyber/message/raw_message.h"
 
+#include "modules/common/util/message_util.h"
+#include "modules/map/hdmap/hdmap_util.h"
+#include "modules/map/pnc_map/pnc_map.h"
+#include "modules/planning/common/planning_context.h"
+
 #include "modules/canbus/proto/chassis.pb.h"
 #include "modules/localization/proto/localization.pb.h"
+#include "modules/perception/proto/perception_obstacle.pb.h"
 #include "modules/perception/proto/traffic_light_detection.pb.h"
-#include "modules/planning/common/planning_gflags.h"
-#include "modules/planning/proto/pad_msg.pb.h"
 #include "modules/planning/proto/planning.pb.h"
-#include "modules/planning/proto/planning_config.pb.h"
 #include "modules/prediction/proto/prediction_obstacle.pb.h"
 #include "modules/routing/proto/routing.pb.h"
+
+using apollo::canbus::Chassis;
+using apollo::common::time::Clock;
+using apollo::hdmap::HDMapUtil;
+using apollo::localization::LocalizationEstimate;
+using apollo::localization::LocalizationStatus;
+using apollo::perception::PerceptionObstacles;
+using apollo::perception::TrafficLightDetection;
+using apollo::planning::ADCTrajectory;
+using apollo::prediction::PredictionObstacles;
+using apollo::relative_map::MapMsg;
+using apollo::routing::RoutingRequest;
+using apollo::routing::RoutingResponse;
 
 namespace apollo {
 namespace fuzzing {
@@ -37,29 +59,33 @@ class FuzzingComponent final
   void CheckRerouting();
   bool CheckInput();
 
+  std::shared_ptr<apollo::cyber::Node> fuzzer_node_;
+  std::mutex mutex_;
+
   // Readers
-  std::shared_ptr<cyber::Reader<perception::TrafficLightDetection>> traffic_light_reader_;
-  std::shared_ptr<cyber::Reader<localization::LocalizationEstimate>> localization_reader_;
-  std::shared_ptr<cyber::Reader<routing::RoutingRequest>> routing_request_reader_;
-  std::shared_ptr<cyber::Reader<routing::RoutingResponse>> routing_response_reader_;
-  std::shared_ptr<cyber::Reader<planning::PadMessage>> pad_message_reader_;
-  std::shared_ptr<cyber::Reader<relative_map::MapMsg>> relative_map_reader_;
+  std::shared_ptr<cyber::Reader<Chassis>> chassis_reader_;
+  std::shared_ptr<cyber::Reader<LocalizationEstimate>> localization_reader_;
+  std::shared_ptr<cyber::Reader<LocalizationStatus>> localization_msf_reader_;
+  std::shared_ptr<cyber::Reader<PerceptionObstacles>> perception_obstacles_reader_;
+  std::shared_ptr<cyber::Reader<TrafficLightDetection>> traffic_light_reader_;
+  std::shared_ptr<cyber::Reader<ADCTrajectory>> planning_trajectory_reader_;
+  std::shared_ptr<cyber::Reader<PredictionObstacles>> prediction_obstacles_reader_;
+  std::shared_ptr<cyber::Reader<MapMsg>> relative_map_reader_;
+  std::shared_ptr<cyber::Reader<RoutingRequest>> routing_request_reader_;
+  std::shared_ptr<cyber::Reader<RoutingResponse>> routing_response_reader_;
 
   // Writers
-  std::shared_ptr<cyber::Writer<planning::ADCTrajectory>> planning_writer_;
-  std::shared_ptr<cyber::Writer<routing::RoutingRequest>> rerouting_writer_;
+  std::shared_ptr<cyber::Writer<Chassis>> chassis_writer_;
+  std::shared_ptr<cyber::Writer<LocalizationEstimate>> localization_writer_;
+  std::shared_ptr<cyber::Writer<LocalizationStatus>> localization_msf_writer_;
+  std::shared_ptr<cyber::Writer<PerceptionObstacles>> perception_obstacles_writer_;
+  std::shared_ptr<cyber::Writer<TrafficLightDetection>> traffic_light_writer_;
+  std::shared_ptr<cyber::Writer<ADCTrajectory>> planning_trajectory_writer_;
+  std::shared_ptr<cyber::Writer<PredictionObstacles>> prediction_obstacles_writer_;
+  std::shared_ptr<cyber::Writer<MapMsg>> relative_map_writer_;
+  std::shared_ptr<cyber::Writer<RoutingRequest>> routing_request_writer_;
+  std::shared_ptr<cyber::Writer<RoutingResponse>> routing_response_writer_;
 
-  std::mutex mutex_;
-  perception::TrafficLightDetection traffic_light_;
-  routing::RoutingResponse routing_response_;
-  planning::PadMessage pad_message_;
-  relative_map::MapMsg relative_map_;
-
-//   LocalView local_view_;
-
-//   std::unique_ptr<FuzzingBase> planning_base_;
-
-//   FuzzingConfig config_;
 };
 
 CYBER_REGISTER_COMPONENT(FuzzingComponent)
