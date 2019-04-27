91a83bfb4eb21f2afa44292493b2935755b4a071
smlight <imzy140@gmail.com>
2019-04-27 10:59:15 +0800

    3

--- modules/fuzzing/gen.py
+++ modules/fuzzing/gen.py
@@ -7,9 +7,10 @@ message_fuzzers = protofuzz.from_file('modules/fuzzing/proto/routing_fuzzed.prot
 
 for i in range(1, 31):
     i_leading0 = str(i).rjust(3, '0')
-    cnt = random.randint(1, 19)
+    cnt = random.randint(1, 5)
     for obj in message_fuzzers['RoutingRequest{}'.format(i_leading0)].permute():
         cnt -= 1
+        print(cnt)
         if cnt == 0:
             with open('modules/fuzzing/proto/routing{}.ascii'.format(i_leading0), 'w') as fo:
                 fo.write(str(obj))
