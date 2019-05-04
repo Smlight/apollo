5306c0275fc71c25af150cb4c6991f8240f9a143
smlight <imzy140@gmail.com>
2019-05-04 16:29:50 +0800

    use wrap function

--- modules/routing/routing_main.cc
+++ modules/routing/routing_main.cc
@@ -47,6 +47,41 @@ using protobuf_mutator::RandomEngine;
 using std::string;
 using std::vector;
 
+google::protobuf::Message* get_class_(int n)
+{
+  if (n==001) return new apollo::routing::RoutingRequest001();
+  else if (n==002) return new apollo::routing::RoutingRequest002();
+  else if (n==003) return new apollo::routing::RoutingRequest003();
+  else if (n==004) return new apollo::routing::RoutingRequest004();
+  else if (n==005) return new apollo::routing::RoutingRequest005();
+  else if (n==006) return new apollo::routing::RoutingRequest006();
+  else if (n==007) return new apollo::routing::RoutingRequest007();
+  else if (n==008) return new apollo::routing::RoutingRequest008();
+  else if (n==009) return new apollo::routing::RoutingRequest009();
+  else if (n==010) return new apollo::routing::RoutingRequest010();
+  else if (n==011) return new apollo::routing::RoutingRequest011();
+  else if (n==012) return new apollo::routing::RoutingRequest012();
+  else if (n==013) return new apollo::routing::RoutingRequest013();
+  else if (n==014) return new apollo::routing::RoutingRequest014();
+  else if (n==015) return new apollo::routing::RoutingRequest015();
+  else if (n==016) return new apollo::routing::RoutingRequest016();
+  else if (n==017) return new apollo::routing::RoutingRequest017();
+  else if (n==018) return new apollo::routing::RoutingRequest018();
+  else if (n==019) return new apollo::routing::RoutingRequest019();
+  else if (n==020) return new apollo::routing::RoutingRequest020();
+  else if (n==021) return new apollo::routing::RoutingRequest021();
+  else if (n==022) return new apollo::routing::RoutingRequest022();
+  else if (n==023) return new apollo::routing::RoutingRequest023();
+  else if (n==024) return new apollo::routing::RoutingRequest024();
+  else if (n==025) return new apollo::routing::RoutingRequest025();
+  else if (n==026) return new apollo::routing::RoutingRequest026();
+  else if (n==027) return new apollo::routing::RoutingRequest027();
+  else if (n==028) return new apollo::routing::RoutingRequest028();
+  else if (n==029) return new apollo::routing::RoutingRequest029();
+  else if (n==030) return new apollo::routing::RoutingRequest030();
+  return apollo::routing::RoutingRequest();
+}
+
 int main(int argc, char** argv) {
   // parse the argument
   ModuleArgument module_args;
@@ -64,60 +99,28 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  vector<::google::protobuf::Message*> routing_vec;
-  routing_vec.push_back(new apollo::routing::RoutingRequest001());
-  routing_vec.push_back(new apollo::routing::RoutingRequest002());
-  routing_vec.push_back(new apollo::routing::RoutingRequest003());
-  routing_vec.push_back(new apollo::routing::RoutingRequest004());
-  routing_vec.push_back(new apollo::routing::RoutingRequest005());
-  routing_vec.push_back(new apollo::routing::RoutingRequest006());
-  routing_vec.push_back(new apollo::routing::RoutingRequest007());
-  routing_vec.push_back(new apollo::routing::RoutingRequest008());
-  routing_vec.push_back(new apollo::routing::RoutingRequest009());
-  routing_vec.push_back(new apollo::routing::RoutingRequest010());
-  routing_vec.push_back(new apollo::routing::RoutingRequest011());
-  routing_vec.push_back(new apollo::routing::RoutingRequest012());
-  routing_vec.push_back(new apollo::routing::RoutingRequest013());
-  routing_vec.push_back(new apollo::routing::RoutingRequest014());
-  routing_vec.push_back(new apollo::routing::RoutingRequest015());
-  routing_vec.push_back(new apollo::routing::RoutingRequest016());
-  routing_vec.push_back(new apollo::routing::RoutingRequest017());
-  routing_vec.push_back(new apollo::routing::RoutingRequest018());
-  routing_vec.push_back(new apollo::routing::RoutingRequest019());
-  routing_vec.push_back(new apollo::routing::RoutingRequest020());
-  routing_vec.push_back(new apollo::routing::RoutingRequest021());
-  routing_vec.push_back(new apollo::routing::RoutingRequest022());
-  routing_vec.push_back(new apollo::routing::RoutingRequest023());
-  routing_vec.push_back(new apollo::routing::RoutingRequest024());
-  routing_vec.push_back(new apollo::routing::RoutingRequest025());
-  routing_vec.push_back(new apollo::routing::RoutingRequest026());
-  routing_vec.push_back(new apollo::routing::RoutingRequest027());
-  routing_vec.push_back(new apollo::routing::RoutingRequest028());
-  routing_vec.push_back(new apollo::routing::RoutingRequest029());
-  routing_vec.push_back(new apollo::routing::RoutingRequest030());
-
   auto fuzzer_node = apollo::cyber::CreateNode("fuzzer");
-  auto routing_request_writer =
-      fuzzer_node->CreateWriter<RoutingRequest>(FLAGS_routing_request_topic);
+  auto routing_request_writer = fuzzer_node->CreateWriter<RoutingRequest>(FLAGS_routing_request_topic);
 
   srand(time(NULL));
   Mutator mutator(new RandomEngine(rand()));
 
+  vector<google::protobuf::Message*> routing_vec;
+  char buf[16];
   for (int i = 0; i < 30; i++) {
-    char buf[8];
     sprintf(buf, "%03d", i + 1);
     string i_leading0(buf);
-    GetProtoFromASCIIFile(
-        "/apollo/modules/fuzzing/proto/routing" + i_leading0 + ".ascii",
-        routing_vec[i]);
+    routing_vec[i] = get_class_(i + 1);
+    GetProtoFromASCIIFile("/apollo/modules/fuzzing/proto/routing" + i_leading0 + ".ascii", routing_vec[i]);
   }
 
   Rate rate(1.0);
   while (apollo::cyber::OK()) {
     int idx = rand() % 30;
     mutator.Mutate(routing_vec[idx], 4096);
-    routing_request_writer->Write(static_cast<std::shared_ptr<RoutingRequest>>(
-        reinterpret_cast<RoutingRequest*>(routing_vec[idx])));
+    auto now = get_class_(idx + 1);
+    *now = *routing_vec[idx];
+    routing_request_writer->Write(std::shared_ptr<RoutingRequest>(reinterpret_cast<RoutingRequest*>(now)));
     rate.Sleep();
   }
 
