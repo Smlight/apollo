0ccd5dc89233e863a6a8c52834d5c3c4912dde01
Smlight <imzy140@gmail.com>
2019-05-30 22:06:57 +0800

    test mutator

--- modules/fuzzing/fuzzing_component.cc
+++ modules/fuzzing/fuzzing_component.cc
@@ -7,7 +7,14 @@
 
 #include "modules/fuzzing/fuzzing_component.h"
 
+using apollo::cyber::common::GetProtoFromASCIIFile;
+using apollo::cyber::common::GetProtoFromBinaryFile;
 using apollo::fuzzing::FuzzMessage;
+using apollo::routing::RoutingRequest;
+using protobuf_mutator::Mutator;
+using protobuf_mutator::RandomEngine;
+using std::string;
+using std::vector;
 
 namespace apollo {
 namespace fuzzing {
@@ -76,10 +83,7 @@ void FuzzingComponent::InitReaders() {
       });
 }
 
-bool FuzzingComponent::Init() {
-  fuzzer_node_ = apollo::cyber::CreateNode("fuzzer");
-
-  InitReaders();
+void FuzzingComponent::InitWriters() {
   chassis_writer_ = fuzzer_node_->CreateWriter<Chassis>(FLAGS_chassis_topic);
   localization_writer_ = fuzzer_node_->CreateWriter<LocalizationEstimate>(
       FLAGS_localization_topic);
@@ -100,6 +104,18 @@ bool FuzzingComponent::Init() {
       fuzzer_node_->CreateWriter<RoutingRequest>(FLAGS_routing_request_topic);
   routing_response_writer_ =
       fuzzer_node_->CreateWriter<RoutingResponse>(FLAGS_routing_response_topic);
+}
+
+bool FuzzingComponent::Init() {
+  fuzzer_node_ = apollo::cyber::CreateNode("fuzzer");
+
+  InitReaders();
+  InitWriters();
+  
+  RoutingRequest* routing_request_p;
+  GetProtoFromASCIIFile("/apollo/modules/routing/routing.ascii",
+                        routing_request_p);
+  message_p_ = (google::protobuf::Message*)routing_request_p;
 
   return true;
 }
@@ -107,12 +123,10 @@ bool FuzzingComponent::Init() {
 bool FuzzingComponent::Proc(
     const std::shared_ptr<localization::LocalizationEstimate>&
         localization_estimate) {
-  FuzzMessage* fuzz_message_p = new FuzzMessage();
-  fuzz_message_p->set_msg(std::string(1024, 'e'));
-  //   chassis_writer_->Write(static_cast<std::shared_ptr<Chassis>>(reinterpret_cast<Chassis*>(fuzz_message_p)));
-  traffic_light_writer_->Write(
-      static_cast<std::shared_ptr<TrafficLightDetection>>(
-          reinterpret_cast<TrafficLightDetection*>(fuzz_message_p)));
+  Mutator mutator(new RandomEngine(1));
+  mutator.Mutate(message_p_, 4096);
+  routing_request_writer_->Write(static_cast<std::shared_ptr<RoutingRequest>>(
+      reinterpret_cast<RoutingRequest*>(&message_p_)));
   return true;
 }
 
